49da99800db6c04372744dff6e2489760cd379f9
Karsten Jakobsen <karsten@karstenjakobsen.dk>
2025-10-15 10:51:08 +0000

    feat: Key name

--- aws-ec2-v2/terraform/module-compute/instance.tf
+++ aws-ec2-v2/terraform/module-compute/instance.tf
@@ -8,8 +8,9 @@ resource "aws_instance" "ec2" {
   subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
   disable_api_termination     = false
-  user_data = file("${path.module}/templates/ignition.json")
+  # user_data = file("${path.module}/templates/ignition.json")
   key_name = "kj"
+  user_data_replace_on_change = true
 
   root_block_device {
     volume_size           = var.vm_disk_size
